fdf9f390c84a9dbe3b0492b6f8306aeece848a54
ward <wardazo@gmail.com>
2022-03-18 09:42:23 +0100

    initial

---
+++ mod_chatwith_1.0.0/mod_chatwith.php
@@ -0,0 +1,7 @@
+<?php
+// No direct access
+defined('_JEXEC') or die;
+
+$widgetId = $params->get('widget_id');
+require JModuleHelper::getLayoutPath('mod_chatwith');
+
---
+++ mod_chatwith_1.0.0/tmpl/default.php
@@ -0,0 +1,5 @@
+<?php 
+// No direct access
+defined('_JEXEC') or die; ?>
+
+<script defer src="https://widget.tochat.be/bundle.js?key=<?php echo $widgetId; ?>"></script>
